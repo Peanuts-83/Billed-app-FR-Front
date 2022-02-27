cf993d2133c0b48568629fe08b8e8f6996d70f0d
Peanuts-83 <tranque@free.fr>
2022-02-27 17:41:59 +0100

    Bug #4 Dashboard - handleEditTicket - solved

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,8 +86,15 @@ export default class {
     }
 
     handleEditTicket(e, bill, bills) {
-        if (this.counterBill === undefined || this.id !== bill.id) this.counterBill = 0
-        if (this.id === undefined || this.id !== bill.id) this.id = bill.id
+        if (this.counterBill === undefined) this.counterBill = 0
+        if ($('.dashboard-right-container div').has('#big-billed-icon').length == 0) {
+            this.bill.id != e.delegateTarget.id.replace('open-bill', '') ?
+                this.counterBill = 0 : this.counterBill = 1
+        } else {
+            this.counterBill = 0
+        }
+
+        this.bill = bill
 
         if (this.counterBill % 2 === 0) {
             bills.forEach(b => {
@@ -96,16 +103,12 @@ export default class {
             $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
             $('.dashboard-right-container div').html(DashboardFormUI(bill))
             $('.vertical-navbar').css({ height: '150vh' })
-            this.counterBill++
         } else {
             $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
-
-            $('.dashboard-right-container div').html(`
-        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-      `)
+            $('.dashboard-right-container div').html(`<div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>`)
             $('.vertical-navbar').css({ height: '120vh' })
-            this.counterBill++
         }
+
         $('#icon-eye-d').click(this.handleClickIconEye)
         $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
         $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -133,21 +136,22 @@ export default class {
 
     handleShowTickets(e, bills, index) {
         if (this.counter === undefined) this.counter = 0
-        this.index = index
-        $(`#status-bills-container${this.index}`).find('.bill-card').length == 0 ? this.counter = 0 : this.counter = 1
+        // this.index = index
+        $(`#status-bills-container${index}`).find('.bill-card').length == 0 ? this.counter = 0 : this.counter = 1
 
-        console.log(this.counter%2 == 0 ? 'SHOW' : 'HIDE', 'Counter set', this.counter, 'Index set', this.index)
+        console.log(this.counter % 2 == 0 ? 'SHOW' : 'HIDE', 'Counter show', this.counter, 'Index show', this.index)
 
         if (this.counter % 2 === 0) {
-            $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' })
-            $(`#status-bills-container${this.index}`).html(cards(filteredBills(bills, getStatus(this.index))))
+            $(`#arrow-icon${index}`).css({ transform: 'rotate(0deg)' })
+            $(`#status-bills-container${index}`).html(cards(filteredBills(bills, getStatus(index))))
         } else {
-            $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' })
-            $(`#status-bills-container${this.index}`).html("")
+            $(`#arrow-icon${index}`).css({ transform: 'rotate(90deg)' })
+            $(`#status-bills-container${index}`).html("")
         }
 
-        this.counter++
+        // this.counter++
         bills.forEach(bill => {
+            $(`#open-bill${bill.id}`).off('click')
             $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
         })
 
